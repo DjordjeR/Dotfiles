5bb9e30061f4eea4e2bbfdeede41b78491698d80
Rajic, Djordje <rajicdj@gmail.com>
2025-12-13 11:14:08 +0100

    ADD: Some changes

---
+++ dot_wezterm.lua
@@ -0,0 +1,34 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+local is_windows = wezterm.target_triple:find("windows") ~= nil
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- Appearance
+config.font = wezterm.font("JetBrainsMono Nerd Font")
+config.font_size = 12
+config.window_background_opacity = 0.95
+config.color_scheme = "nightfox"
+
+-- OS specific startup shell
+if is_windows then
+    config.default_prog = { "pwsh.exe", "-NoLogo" }
+else
+    config.default_prog = { "/usr/bin/zsh", "-l" }
+end
+
+-- Launch options similar to Windows Terminal
+
+config.launch_menu = {}
+
+if is_windows then
+    -- TODO:
+else
+    table.insert(config.launch_menu, {
+        label = "Default",
+        args = { "/usr/bin/zsh", "-l" },
+    })
+end
+
+return config

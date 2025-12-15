9ecc22677cee3b62160b8bd086ceebfbb9d95759
Rajic, Djordje <djordje@rajic.dev>
2025-12-15 21:23:51 +0100

    Transparency config

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -8,15 +8,16 @@ local config = wezterm.config_builder()
 -- Appearance
 config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.font_size = 12
-config.window_background_opacity = 0.9
 config.color_scheme = "nightfox"
 config.hide_tab_bar_if_only_one_tab = true
 
 -- OS specific startup shell
 if is_windows then
 	config.default_prog = { "pwsh.exe", "-NoLogo" }
+	config.window_background_opacity = 1
 else
 	config.default_prog = { "/usr/bin/zsh", "-l" }
+	config.window_background_opacity = 0.9
 end
 
 -- Launch options similar to Windows Terminal
